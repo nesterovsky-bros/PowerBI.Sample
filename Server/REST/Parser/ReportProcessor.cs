26839c9f44171800a6137e4657f0a16173a68175
Vladimir Nesterovsky <vladimir@nesterovsky-bros.com>
2022-08-03 22:50:01 +0300

    Parser sample

---
+++ Server/REST/Parser/ReportProcessor.cs
@@ -0,0 +1,53 @@
+using System.Xml.Linq;
+
+namespace REST.Parser;
+
+using static Functions;
+
+public record Page(int page, string[] lines);
+
+public record ReportLine(int page, int row, string text);
+
+public record ReportData(int report, Page head, IEnumerable<ReportLine> lines);
+
+public class ReportProcessor
+{
+  public static IEnumerable<XElement> Parse(
+    IEnumerable<string> lines,
+    Func<ReportData, IEnumerable<XElement>> reportHandler)
+  {
+    var pages = lines.
+      // Form groups of lines.
+      // GroupAdjacent(this IEnumeration<T>) => IEnumeration<(T head, IEnumeration<T> items)>
+      // Note: group items must be consumed before processing next group.
+      GroupAdjacent(startsAt: line => line.StartsWith("1")).
+      // Create enumeration of pages.
+      Select((group, index) =>
+        new Page(page: index + 1, lines: group.items.ToArray()));
+
+    var reports = pages.
+      // Form enumeration of tuples (int report, Page page)
+      Select(page => (report: int.Parse(Substring(page.lines[0], 70, 7)), page)).
+      // For goups of pages per report.
+      GroupAdjacent(item => item.report).
+      // Create a enumeration of ReportData.
+      Select(group => new ReportData(
+        // Report number.
+        report: group.head.report,
+        // First page of the report.
+        head: group.head.page,
+        // Enumeration of ReportLine - all lines of report without page headers.
+        lines: group.items.SelectMany(report =>
+          report.page.lines.Skip(2).Select((line, index) =>
+            new ReportLine(
+              page: report.page.page,
+              row: index + 3,
+              text: line)))));
+
+    return reports.
+      // Process report data with specific handler.
+      Select(reportHandler).
+      // produce final enumeration of XElement(s).
+      SelectMany(item => item);
+  }
+}
